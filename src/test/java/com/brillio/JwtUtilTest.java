fc10f2141ef9699bb3bfda50ae1b09021a4b3d53
KGSSV AKHIL KUMAR <53571917+KGSSV@users.noreply.github.com>
2023-07-29 19:12:01 +0530

    latest test case for JwtUtil

--- src/test/java/com/brillio/JwtUtilTest.java
+++ src/test/java/com/brillio/JwtUtilTest.java
@@ -1,84 +1,59 @@
 package com.brillio;
 
 import com.brillio.util.JwtUtil;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Date;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
-class JwtUtilTest {
+public class JwtUtilTest {
 
     private JwtUtil jwtUtil;
     private UserDetails userDetails;
 
     @BeforeEach
-    void setUp() {
+    public void setup() {
         jwtUtil = new JwtUtil();
-        userDetails = new UserDetails() {
-            @Override
-            public String getPassword() {
-                return null;
-            }
-
-            @Override
-            public String getUsername() {
-                return "testUser";
-            }
-
-            @Override
-            public boolean isAccountNonExpired() {
-                return false;
-            }
-
-            @Override
-            public boolean isAccountNonLocked() {
-                return false;
-            }
-
-            @Override
-            public boolean isCredentialsNonExpired() {
-                return false;
-            }
-
-            @Override
-            public boolean isEnabled() {
-                return false;
-            }
-        };
+        userDetails = mock(UserDetails.class);
+        when(userDetails.getUsername()).thenReturn("testUser");
     }
 
     @Test
-    void testExtractUsername() {
+    public void testExtractUsername() {
         String token = jwtUtil.generateToken(userDetails);
         String username = jwtUtil.extractUsername(token);
-        assertEquals("testUser", username);
+        Assertions.assertEquals("testUser", username);
     }
 
     @Test
-    void testExtractExpiration() {
+    public void testExtractExpiration() {
         String token = jwtUtil.generateToken(userDetails);
         Date expiration = jwtUtil.extractExpiration(token);
-        assertNotNull(expiration);
+        Assertions.assertNotNull(expiration);
     }
 
     @Test
-    void testIsTokenExpired() {
+    public void testIsTokenExpired() {
         String token = jwtUtil.generateToken(userDetails);
-        assertFalse(jwtUtil.isTokenExpired(token));
+        boolean isExpired = jwtUtil.isTokenExpired(token);
+        Assertions.assertFalse(isExpired);
     }
 
     @Test
-    void testGenerateToken() {
+    public void testGenerateToken() {
         String token = jwtUtil.generateToken(userDetails);
-        assertNotNull(token);
+        Assertions.assertNotNull(token);
     }
 
     @Test
-    void testValidateToken() {
+    public void testValidateToken() {
         String token = jwtUtil.generateToken(userDetails);
-        assertTrue(jwtUtil.validateToken(token, userDetails));
+        boolean isValid = jwtUtil.validateToken(token, userDetails);
+        Assertions.assertTrue(isValid);
     }
 }
