b9851601d898392281ba0016e5cadfbcea02d9c9
umang93769 <42294165+umang93769@users.noreply.github.com>
2023-07-29 19:06:56 +0530

    Update LoginTest.java

--- src/functional /LoginTest.java
+++ src/functional /LoginTest.java
@@ -1,49 +1,84 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+package com.brillio;
 
-public class LoginTest {
-    public static void main(String[] args) throws InterruptedException {
-        // Step 1: Set up Chrome WebDriver with the appropriate driver executable path
-        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+import com.brillio.util.JwtUtil;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.security.core.userdetails.UserDetails;
 
-        // Step 2: Open the Chrome browser
-        WebDriver driver = new ChromeDriver();
+import java.util.Date;
 
-        // Step 3: Navigate to the Smart Bank login page
-        String baseUrl = "http://35.175.173.214:8081/"; // Replace with the actual login page URL
-        System.out.println("Opening The Browser");
-        driver.get(baseUrl);
-        System.out.println("Opening The URL: " + baseUrl);
-	    Thread.sleep(5000);
+import static org.junit.jupiter.api.Assertions.*;
 
-        // Step 4: Enter incorrect credentials
-        String invalidUsername = "incorrect_username";
-        String invalidPassword = "incorrect_password";
-        System.out.println("Entering incorrect user with username: " + invalidUsername + " and password: " + invalidPassword);
-        WebElement inputUsername = driver.findElement(By.id("username"));
-        WebElement inputPassword = driver.findElement(By.id("password"));
-        inputUsername.sendKeys(invalidUsername);
-	    Thread.sleep(5000);
-        inputPassword.sendKeys(invalidPassword);
-	    Thread.sleep(5000);
+class JwtUtilTest {
 
-        // Step 5: Click the "Login" button and verify error message display
-        WebElement loginButton = driver.findElement(By.xpath("//button[text()='Login']"));
-        loginButton.click();
-        Thread.sleep(5000); // Adjust the wait time as needed
+    private JwtUtil jwtUtil;
+    private UserDetails userDetails;
 
-        WebElement errorMessageElement = driver.findElement(By.className("error-message"));
-        if (errorMessageElement.isDisplayed()) {
-            String errorMessage = errorMessageElement.getText();
-            System.out.println("Error message displayed: " + errorMessage);
-        } else {
-            System.out.println("Error Message Element is not displayed.");
-        }
+    @BeforeEach
+    void setUp() {
+        jwtUtil = new JwtUtil();
+        userDetails = new UserDetails() {
+            @Override
+            public String getPassword() {
+                return null;
+            }
 
-        // Step 6: Close the browser and quit the WebDriver
-        System.out.println("Closing the Browser");
-        driver.quit();
+            @Override
+            public String getUsername() {
+                return "testUser";
+            }
+
+            @Override
+            public boolean isAccountNonExpired() {
+                return false;
+            }
+
+            @Override
+            public boolean isAccountNonLocked() {
+                return false;
+            }
+
+            @Override
+            public boolean isCredentialsNonExpired() {
+                return false;
+            }
+
+            @Override
+            public boolean isEnabled() {
+                return false;
+            }
+        };
+    }
+
+    @Test
+    void testExtractUsername() {
+        String token = jwtUtil.generateToken(userDetails);
+        String username = jwtUtil.extractUsername(token);
+        assertEquals("testUser", username);
+    }
+
+    @Test
+    void testExtractExpiration() {
+        String token = jwtUtil.generateToken(userDetails);
+        Date expiration = jwtUtil.extractExpiration(token);
+        assertNotNull(expiration);
+    }
+
+    @Test
+    void testIsTokenExpired() {
+        String token = jwtUtil.generateToken(userDetails);
+        assertFalse(jwtUtil.isTokenExpired(token));
+    }
+
+    @Test
+    void testGenerateToken() {
+        String token = jwtUtil.generateToken(userDetails);
+        assertNotNull(token);
+    }
+
+    @Test
+    void testValidateToken() {
+        String token = jwtUtil.generateToken(userDetails);
+        assertTrue(jwtUtil.validateToken(token, userDetails));
     }
 }
