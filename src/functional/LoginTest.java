bf31046d2e4f2bd8d24ac9ff9a3e1e06dbebabea
umang93769 <42294165+umang93769@users.noreply.github.com>
2023-07-29 22:32:19 +0530

    Update LoginTest.java

--- src/functional/LoginTest.java
+++ src/functional/LoginTest.java
@@ -3,7 +3,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class SmartBankLoginAutomation {
+public class LoginTest {
 
     public static void main(String[] args) throws InterruptedException {
         // Set the path to the chromedriver executable
